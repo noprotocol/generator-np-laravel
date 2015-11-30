83698f3cbf40b6fef8f49d395d6b9765421d1960
Bob Fanger <bfanger@gmail.com>
2015-11-30 15:53:42 +0100

    Fix "bold on is not a property of undefined"

--- app/index.js
+++ app/index.js
@@ -57,17 +57,17 @@ NpLaravelGenerator.prototype.animateLogo = function () {
   var cb = this.async();
   this.log.write();
   var count = 35;
-  var no = '\r   NO'.bold;
-  var protocol = 'PROTOCOL'.magenta.bold;
+  var no = chalk.bold('\r   NO');
+  var protocol = chalk.gray.bold('PROTOCOL');
   var self = this;
   var animate = function () {
-    self.log.write(no + (new Array(count).join(' ')) + protocol + ':// '.bold);
+    self.log.write(no + chalk.bold(new Array(count).join(' ')) + protocol + ':// ');
     count--;
     if (count === 0) {
       setTimeout(function () {
         self.log.write(no + protocol + ';// ');
         setTimeout(function () {
-          self.log.write(no + protocol + ':// '.bold);
+          self.log.write(chalk.bold(no + protocol + ':// '));
           setTimeout(function () {
             self.log.write('\n\n');
             cb();
